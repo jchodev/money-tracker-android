5801fac8b7cc4e21595c8105ad7e52c530eeb7fe
jerry <jerry.cho.dev@hotmail.com>
2024-04-19 11:18:38 +0100

    Release to google play

--- buildSrc/src/main/java/CommonModulePlugin.kt
+++ buildSrc/src/main/java/CommonModulePlugin.kt
@@ -46,7 +46,7 @@ private fun basicApply(project: Project, withJetpack: Boolean = true){
             defaultConfig {
                 targetSdk = 34
                 minSdk = 24
-                versionCode = 1
+                versionCode = 2
                 versionName = "1.0"
                 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
             }
